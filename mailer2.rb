cbcf1c76e62d8194a0aa3508972659c5b471a461
Daniel Sjösten <dansjo_87@hotmail.com>
2018-05-06 08:50:14 +0200

    slaktar kod

--- mailer2.rb
+++ mailer2.rb
@@ -17,12 +17,32 @@ Subject: #{subject}
     #{body}
 
 EOF
-    send_email from, to, mailtext
+
+    @smtp_info =
+        begin
+          YAML.load_file("C:\\Users\\Daniel\\Documents\\blocketscrape\\cred.yaml")
+        rescue
+          $stderr.puts "Could not find SMTP info"
+          exit -1
+        end
+
+    begin
+      Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
+      Net::SMTP.start(@smtp_info[:smtp_server], @smtp_info[:port], @smtp_info[:helo], @smtp_info[:username], @smtp_info[:password], @smtp_info[:authentication]) do |smtp|
+        smtp.send_message mailtext, from, to
+      end
+
+    rescue => e
+      raise "Exception occured: #{e} "
+      exit -1
+    end
+      #send_email from, to, mailtext
   end
 
 
 
-  
+
   private
   def send_email from, to, mailtext
 
